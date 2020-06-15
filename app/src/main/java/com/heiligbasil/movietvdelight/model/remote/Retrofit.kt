304b541f1b0d8631551c1e66370a417af87df98d
heiligbasil <basilief@yahoo.com>
2020-06-15 00:23:46 -0700

    Add getter for poster image URL

--- app/src/main/java/com/heiligbasil/movietvdelight/model/remote/Retrofit.kt
+++ app/src/main/java/com/heiligbasil/movietvdelight/model/remote/Retrofit.kt
@@ -6,24 +6,28 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.net.URL
 import java.util.concurrent.TimeUnit
 
 class Retrofit {
 
     companion object {
 
-        val apiKey = BuildConfig.API_KEY
-        val baseUrl = "https://api.themoviedb.org/"
+        const val apiKey = BuildConfig.API_KEY
+        const val baseUrl = "https://api.themoviedb.org/"
+        const val baseImageUrl = "https://image.tmdb.org/t/p/"
+        const val posterSize = "w154/"
+        const val backdropSize = "w500"
 
         val interceptor = HttpLoggingInterceptor().apply {
-            this.level=HttpLoggingInterceptor.Level.BODY
+            this.level = HttpLoggingInterceptor.Level.BODY
         }
 
         val client = OkHttpClient.Builder().apply {
             this.addInterceptor(interceptor)
-                .connectTimeout(20,TimeUnit.SECONDS)
-                .readTimeout(20,TimeUnit.SECONDS)
-                .writeTimeout(20,TimeUnit.SECONDS)
+                .connectTimeout(20, TimeUnit.SECONDS)
+                .readTimeout(20, TimeUnit.SECONDS)
+                .writeTimeout(20, TimeUnit.SECONDS)
         }.build()
 
         fun getInstance(): Retrofit {
@@ -32,6 +36,13 @@ class Retrofit {
                 .client(client)
                 .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
                 .build()
+        }
+
+        fun buildPosterUrl(filename: String): String {
+            val url1 = URL(baseImageUrl)
+            val url2 = URL(url1, posterSize)
+            val url3 = URL(url2, filename.removePrefix("/"))
+            return url3.toString()
         }
     }
 }
