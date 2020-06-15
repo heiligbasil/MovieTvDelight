8c03534b351552e8a3073642490347d479f1f490
heiligbasil <basilief@yahoo.com>
2020-06-14 19:50:55 -0700

    Add Tab Layout listener with observation of the Top Rated Movies

--- app/src/main/java/com/heiligbasil/movietvdelight/view/BrowseFragment.kt
+++ app/src/main/java/com/heiligbasil/movietvdelight/view/BrowseFragment.kt
@@ -1,10 +1,19 @@
 package com.heiligbasil.movietvdelight.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.liveData
+import com.google.android.material.tabs.TabLayout
 import com.heiligbasil.movietvdelight.R
+import com.heiligbasil.movietvdelight.model.entities.MovieTopRated
+import com.heiligbasil.movietvdelight.model.remote.Retrofit
+import com.heiligbasil.movietvdelight.model.remote.MtdService
+import kotlinx.android.synthetic.main.fragment_browse.*
+import retrofit2.Response
 
 class BrowseFragment : OptionsMenuFragment() {
     override fun onCreateView(
@@ -18,6 +27,43 @@ class BrowseFragment : OptionsMenuFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        browse_tab_layout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                onTabSelected(tab)
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+
+                val tmdbService = Retrofit.getInstance().create(MtdService::class.java)
+
+                if (tab?.text.toString().equals(getString(R.string.browse_top_rated_movies))) {
+
+                    val mtrResponse = liveData<Response<MovieTopRated>> {
+                        val response = tmdbService.getTopRatedMovies(Retrofit.apiKey, "enUS", 1)
+                        emit(response)
+                    }
+
+                    mtrResponse.observe(viewLifecycleOwner, Observer {
+                        val mtr = it.body()
+                        val topRatedMovies = mtr?.results?.listIterator()
+                        if (topRatedMovies !== null) {
+                            while (topRatedMovies.hasNext()) {
+                                val topRatedMovie = topRatedMovies.next()
+                                Log.i("test321", topRatedMovie.title)
+                            }
+                        }
+                    })
+                } else {
+
+                }
+            }
+
+        })
+
+        browse_tab_layout.selectTab(browse_tab_layout.getTabAt(0))
 
     }
 
