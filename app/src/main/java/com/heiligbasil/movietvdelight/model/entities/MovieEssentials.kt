c38e9973c9951e9f4b121810509248a8172ea0ef
heiligbasil <basilief@yahoo.com>
2020-06-16 11:56:59 -0700

    Augment entity class with Bindable annotations

--- app/src/main/java/com/heiligbasil/movietvdelight/model/entities/MovieEssentials.kt
+++ app/src/main/java/com/heiligbasil/movietvdelight/model/entities/MovieEssentials.kt
@@ -1,9 +1,14 @@
 package com.heiligbasil.movietvdelight.model.entities
 
+import android.widget.ImageView
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+import androidx.databinding.BindingAdapter
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.google.gson.annotations.SerializedName
+import com.heiligbasil.movietvdelight.model.remote.Retrofit
+import com.squareup.picasso.Picasso
 
 @Entity(tableName = "movie_table")
 data class MovieEssentials(
@@ -14,21 +19,25 @@ data class MovieEssentials(
     @ColumnInfo(name = "movie_backdrop_path")
     val backdropPath: String,
 
-    @ColumnInfo(name = "movie_original_language")
-    val originalLanguage: String,
+    @Bindable
+    @ColumnInfo(name = "movie_language")
+    val language: String,
 
     @ColumnInfo(name = "movie_original_title")
     val originalTitle: String,
 
+    @Bindable
     @ColumnInfo(name = "movie_overview")
     val overview: String,
 
     @ColumnInfo(name = "movie_poster_path")
     val posterPath: String,
 
-    @ColumnInfo(name = "movie_release_date")
-    val releaseDate: String,
+    @Bindable
+    @ColumnInfo(name = "movie_year")
+    val year: String,
 
+    @Bindable
     @ColumnInfo(name = "movie_title")
     val title: String,
 
@@ -37,4 +46,13 @@ data class MovieEssentials(
 
     @ColumnInfo(name = "movie_saved")
     val saved: Boolean
-)
+) : BaseObservable() {
+    companion object {
+        @JvmStatic
+        @BindingAdapter("posterPath")
+        fun loadPoster(imageView: ImageView, url: String) {
+            val posterUrl = Retrofit.buildPosterUrl(url)
+            Picasso.get().load(posterUrl).into(imageView)
+        }
+    }
+}
