a84a7408c438e8272be28933534cc9a2c3c700a9
heiligbasil <basilief@yahoo.com>
2020-06-20 21:15:30 -0700

    Animate background screen in the About fragment

--- app/src/main/java/com/heiligbasil/movietvdelight/view/AboutFragment.kt
+++ app/src/main/java/com/heiligbasil/movietvdelight/view/AboutFragment.kt
@@ -1,11 +1,14 @@
 package com.heiligbasil.movietvdelight.view
 
+
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.heiligbasil.movietvdelight.R
+import kotlinx.android.synthetic.main.fragment_about.*
 
 class AboutFragment : Fragment() {
     override fun onCreateView(
@@ -14,5 +17,15 @@ class AboutFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Start the pulsating animation of the gradient
+        val animatedBackground = fragment_about_parent_layout.background as AnimationDrawable
+        animatedBackground.setEnterFadeDuration(2000)
+        animatedBackground.setExitFadeDuration(3000)
+        animatedBackground.start()
     }
 }
