ff827d429fbaa1627f51ea3aee3f6b33262ca1c4
heiligbasil <basilief@yahoo.com>
2020-06-21 20:38:57 -0700

    Implement the Up button on the Action Bar like it is the Back button

--- app/src/main/java/com/heiligbasil/movietvdelight/view/AboutFragment.kt
+++ app/src/main/java/com/heiligbasil/movietvdelight/view/AboutFragment.kt
@@ -6,11 +6,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.heiligbasil.movietvdelight.R
 import kotlinx.android.synthetic.main.fragment_about.*
 
 class AboutFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Override the default behavior of the Back button
+        val onBackPressedCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().popBackStack()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -27,5 +42,10 @@ class AboutFragment : Fragment() {
         animatedBackground.setEnterFadeDuration(2000)
         animatedBackground.setExitFadeDuration(3000)
         animatedBackground.start()
+
+        // Set up the action bar
+        val appCompatActivity = activity as AppCompatActivity
+        appCompatActivity.supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        appCompatActivity.supportActionBar?.title = "${appCompatActivity.title} - About"
     }
 }
