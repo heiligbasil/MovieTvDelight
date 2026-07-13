f50843c8712b68fc0b21da242afd9eae4df75a6d
heiligbasil <basilief@yahoo.com>
2026-07-13 17:08:31 -0500

    Flesh out tests

--- app/src/androidTest/java/com/heiligbasil/movietvdelight/view/AboutFragmentTest.kt
+++ app/src/androidTest/java/com/heiligbasil/movietvdelight/view/AboutFragmentTest.kt
@@ -1,7 +1,10 @@
 package com.heiligbasil.movietvdelight.view
 
+import androidx.fragment.app.testing.FragmentScenario
+import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import com.heiligbasil.movietvdelight.R
@@ -11,30 +14,70 @@ import org.junit.jupiter.api.Test
 
 class AboutFragmentTest {
 
-    val aboutFragment: AboutFragment = AboutFragment()
-
-    @Test
-    fun testDemonstrateEspressoOnFragment() {
-        onView(withId(R.id.about_text_heading)).check(matches(withText(R.string.app_name)))
-    }
+    private lateinit var scenario: FragmentScenario<AboutFragment>
 
     @BeforeEach
     fun setUp() {
+        scenario = launchFragmentInContainer<AboutFragment>(themeResId = R.style.Theme_MovieTvDelight)
     }
 
     @AfterEach
     fun tearDown() {
+        scenario.close()
     }
 
     @Test
-    fun onCreate() {
+    fun testAppNameHeadingIsDisplayed() {
+        onView(withId(R.id.about_text_heading))
+            .check(matches(isDisplayed()))
     }
 
     @Test
-    fun onCreateView() {
+    fun testAppNameHeadingHasCorrectText() {
+        onView(withId(R.id.about_text_heading))
+            .check(matches(withText(R.string.app_name)))
     }
 
     @Test
-    fun onViewCreated() {
+    fun testLogoImageIsDisplayed() {
+        onView(withId(R.id.about_image_logo))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testTmdbLogoImageIsDisplayed() {
+        onView(withId(R.id.about_image_tmdb))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testCreditsTextIsDisplayed() {
+        onView(withId(R.id.about_text_credits))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testCreditsTextHasCorrectContent() {
+        onView(withId(R.id.about_text_credits))
+            .check(matches(withText(R.string.about_tmdb_credit)))
+    }
+
+    @Test
+    fun testFragmentViewIsCreated() {
+        scenario.onFragment { fragment ->
+            assert(fragment.view != null)
+        }
+    }
+
+    @Test
+    fun testParentLayoutExists() {
+        onView(withId(R.id.fragment_about_parent_layout))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testDemonstrateEspressoOnFragment() {
+        onView(withId(R.id.about_text_heading))
+            .check(matches(withText(R.string.app_name)))
     }
 }
